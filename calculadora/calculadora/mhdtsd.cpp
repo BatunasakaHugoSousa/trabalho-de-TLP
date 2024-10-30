c5cf2dc6e10b2018ba7683e9a66b0506fdb231e3
BatunasakaHugoSousa <huguitodesousa@gmail.com>
2024-10-30 13:23:58 +0000

    Add files via upload

---
+++ calculadora/calculadora/mhdtsd.cpp
@@ -0,0 +1,43 @@
+#include <iostream> 
+#include <locale.h>
+#include <string>
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, "Portuguese");
+	int N, M;
+	char A;
+	cout << ("que calculo quer fazer\nsomar,subtrair,multiplicar ou dividir\nescreva +, -, x, /\n");
+	cin >> A;
+	switch (A) {
+	case '+':
+		cout << ("somar\ndá me dois números\n");
+		cin >> N;
+		cin >> M;
+		cout << (N + M);
+		break;
+	case '-':
+		cout << ("subtrair\ndá me dois números\n");
+		cin >> N;
+		cin >> M;
+		cout << (N - M);
+		break;
+	case 'x':
+	case 'X':
+	case '*':
+		cout << ("multiplicar\ndá me dois números\n");
+		cin >> N;
+		cin >> M;
+		cout << (N * M);
+		break;
+	case '/':
+		cout << ("dividir\ndá me dois números\n");
+		cin >> N;
+		cin >> M;
+		cout << (N / M);
+		break;
+	default:
+		cout << ("tem de ser 1 a 4");
+		break;
+	}
+}
